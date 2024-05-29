73be76aa6edf01d86df5f8c19c586a82f51fff43
Emeric Chevalier <emericchevalier.pro@gmail.com>
2024-05-29 11:34:42 +0200

    build o data

--- examples/quick-comparison.rs
+++ examples/quick-comparison.rs
@@ -15,6 +15,10 @@ const SHARD_BYTES: usize = 64;
 // MAIN
 
 fn main() {
+    for count in [1, 2, 4, 5, 10, 16, 32, 50] {
+			scenarii(count);
+		}
+		/*
     #[cfg(debug_assertions)]
     {
         eprintln!("Warning: Running in debug mode! Please run like this instead: cargo run --release --example quick-comparison");
@@ -22,6 +26,7 @@ fn main() {
 
     println!("                           µs (init)   µs (encode)   µs (decode)");
     println!("                           ---------   -----------   -----------");
+
 
     for count in [3, 32, 64, 128, 256, 512, 1024, 4 * 1024, 32 * 1024] {
         println!("\n{}:{} ({} kiB)", count, count, SHARD_BYTES / 1024);
@@ -35,12 +40,20 @@ fn main() {
             test_reed_solomon_erasure_16(count);
         }
     }
+		*/
 }
 
 // ======================================================================
 // reed-solomon-simd
 
 fn test_reed_solomon_simd(count: usize) {
+    const POINT_SIZE: usize = 2;
+    const N_SHARD: usize = 342;
+    const N_POINT_BATCH: usize = 32;
+    const INPUT_DATA_LEN: usize = POINT_SIZE * N_SHARD * N_POINT_BATCH;
+
+    let count = N_SHARD;
+
     // INIT
 
     let start = Instant::now();
@@ -52,24 +65,14 @@ fn test_reed_solomon_simd(count: usize) {
     // CREATE ORIGINAL
 
     let mut original = vec![vec![0u8; SHARD_BYTES]; count];
-                for o in original.iter_mut() {
-                    for i in 0..2 {
-                        o[i] = i  as u8;
-                    }
-                }
     let mut rng = SmallRng::from_seed([0; 32]);
-	  for original in &mut original {
+    for original in &mut original {
         rng.fill::<[u8]>(original);
     }
 
     println!("Original: {:?}", original);
 
     // ENCODE
-
-		const POINT_SIZE: usize = 2;
-		const N_SHARD: usize = 342;
-		const N_POINT_BATCH: usize = 32;
-		const INPUT_DATA_LEN: usize = POINT_SIZE * N_SHARD * N_POINT_BATCH;
 
     let start = Instant::now();
     let recovery = reed_solomon_simd::encode(count, 2 * count, &original).unwrap();
@@ -90,66 +93,41 @@ fn test_reed_solomon_simd(count: usize) {
     println!("{:14}", elapsed.as_micros());
 
     //const SMALL_SHARD: usize = 2;
-		//nb of point
-    const SMALL_SHARD: usize = 2;
+    //nb of point
     // CHECK
-    for i in 0..count {
-        let mut j = 0;
-        while j < 32 - SMALL_SHARD {
-            let decoder_recovery: Vec<_> = recovery.iter().enumerate().collect();
+    //for i in 0..count {
+    let decoder_recovery: Vec<_> = recovery.iter().enumerate().collect();
 
-            println!("Recovery: {:?}", decoder_recovery);
-            let mut rand_reco: Vec<usize> = (0..(2 * count)).collect();
-            rand_reco.shuffle(&mut rng);
-            rand_reco.truncate(count);
+    println!("Recovery: {:?}", decoder_recovery);
+//    for i in 0..32 {
+        let mut r: Vec<usize> = (0..(2 * count)).collect();
+        r.shuffle(&mut rng);
+        r.truncate(count);
+				let rand_reco = r;
+ //   }
 
-            let restored = reed_solomon_simd::decode(
-                count,
-                2 * count,
-                [(0, ""); 0],
-                rand_reco.into_iter().map(|i| {
-                    let mut r = vec![0; SHARD_BYTES];
-                    r[j..j + SMALL_SHARD]
-                    .copy_from_slice(&decoder_recovery[i].1[j..j + SMALL_SHARD]);
-										r[j + 32..j + 32 + SMALL_SHARD]
-                    .copy_from_slice(&decoder_recovery[i].1[j + 32..j + 32 + SMALL_SHARD]);
- 
-
-                    //                    r[j..j + SMALL_SHARD]
-                    //                       .copy_from_slice(&decoder_recovery[i].1[j..j + SMALL_SHARD]);
-//                    r[j] = decoder_recovery[i].1[j];
-//                    r[j + 32] = decoder_recovery[i].1[j + 32];
-                    (i, r)
-                }),
-            )
-            .unwrap();
-
-            //println!("{}: {} == {}", i, restored[&i][j], original[i][j]);
-            assert_eq!(restored.len(), count);
-            for i in 0..count {
-/*assert_eq!(
-                    restored[&i][j],
-                    original[i][j]
-                );
-assert_eq!(
-                    restored[&i][j + 32],
-                    original[i][j + 32]
-                );
-*/
-                assert_eq!(
-                    restored[&i][j..j + SMALL_SHARD],
-                    original[i][j..j + SMALL_SHARD]
-                );
-                assert_eq!(
-                    restored[&i][j + 32..j + 32 + SMALL_SHARD],
-                    original[i][j + 32 ..j + 32 + SMALL_SHARD]
-                );
-
+    let restored = reed_solomon_simd::decode(
+        count,
+        2 * count,
+        [(0, ""); 0],
+        (0..count).map(|i| {
+					let mut r = vec![0; SHARD_BYTES];
+            // one shard at each pos from any of the distribute points.
+            let ir = rand_reco[i];
+            for j in 0..32 {
+//                let ir = rand_reco[j][i];
+                r[j] = decoder_recovery[ir].1[j];
+                r[j + 32] = decoder_recovery[ir].1[j + 32];
             }
-            j += SMALL_SHARD;
-        }
-    }
 
+//					r=decoder_recovery[i].1.clone();
+            (ir, r)
+        }),
+    )
+    .unwrap();
+
+    //println!("{}: {} == {}", i, restored[&i][j], original[i][j]);
+    assert_eq!(restored.len(), count);
     for i in 0..count {
         assert_eq!(restored[&i], original[i]);
     }
@@ -351,4 +329,61 @@ fn test_reed_solomon_novelpoly(count: usize) {
     // CHECK
 
     assert_eq!(reconstructed, original);
+}
+
+
+const SEGMENT_SIZE: usize = 4096;
+const POINT_SIZE: usize = 2;
+const N_SHARDS: usize = 342;
+const N_POINT_BATCH: usize = 32;
+const SHARD_BATCH_SIZE: usize = N_POINT_BATCH * POINT_SIZE * N_SHARDS;
+
+
+fn build_original(original_data_segments: usize, rng: &mut SmallRng) -> (Vec<u8>, Vec<[u8; SHARD_BYTES]>) {
+	let mut original = vec![0; original_data_segments * SEGMENT_SIZE];
+	rng.fill::<[u8]>(&mut original);
+
+	// every 2 byte chunks get in a point of SHARDBYTES. (losing a few byte, can be optimize later it is
+	// just 8 byte per chunks (342 * 2 * 6 = 4104)).
+	
+	// So for testing best perf have original data chunks multiple of 32.
+	let number_shards_batch = (((original_data_segments * SEGMENT_SIZE) - 1) / SHARD_BATCH_SIZE) + 1;
+
+	println!("{:?}", number_shards_batch);
+	let mut original_shards = vec![[0u8; SHARD_BYTES]; number_shards_batch * N_SHARDS];
+
+	let mut segment_batch = 0;
+	let mut at = 0; // pos in segment batch
+	let mut at2 = 0; // 32 point pos in 64b shards
+	for i in 0..original_data_segments {
+		// runing over segment is totally not necessary: could justrun over original pairs of bytes.
+		let ch = &original[i * SEGMENT_SIZE..(i + 1) * SEGMENT_SIZE];
+		// interleave over N_POINT_BATCH so a single segment can be recover from m fix offset of 64b
+		// shards.
+
+		for b in 0..(ch.len() / 2) {
+				let s = segment_batch * N_SHARDS + at;
+				original_shards[s][at2] = ch[b * 2];
+				original_shards[s][at2 + 32] = ch[b * 2 + 1];
+				at += 1;
+				if at == N_SHARDS {
+					at = 0;
+					at2 += 1;
+					if at2 == 32 {
+						at2 = 0;
+						segment_batch += 1;
+					}
+				}
+		}
+	}
+
+	(original, original_shards)
+}
+
+
+fn scenarii(data_chunks: usize) {
+  let mut rng = SmallRng::from_seed([0; 32]);
+	let (original, o_shards) = build_original(data_chunks, &mut rng);
+	println!("o data size: {:?}", original.len());
+	println!("o sharded size: {:?}", o_shards.len() * SHARD_BYTES);
 }
